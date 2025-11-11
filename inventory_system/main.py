0ba37a575d754fa7a86b219eb7a1e51ab4ccfc86
BhupatiNadar <tamillana2@gmail.com>
2025-11-11 23:36:31 +0530

    Base file

---
+++ inventory_system/main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from modules.login import login
+
+if __name__ == "__main__":
+    window=tk.Tk()
+    login(window)
+    window.mainloop()
---
+++ inventory_system/modules/login.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+def login(window):
+    window.title("login")
+    window.state('zoomed')
+    tk.Label(window,text="")
+    tk.Label()
+        
